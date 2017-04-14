2be87dc7471743ee79677c6616bd08abdd7bb4a3
J4ft <reveillere.pierrick@gmail.com>
2017-04-14 11:59:48 +0200

    s

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -1,9 +1,14 @@
 package fr.unilim.iut.spaceinvaders;
 
+import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
+
 public class SpaceInvaders {
 
+	private static final char MARQUE_VIDE = '.';
+	private static final char MARQUE_VAISSEAU = 'V';
 	int longueur;
 	int hauteur;
+	Vaisseau vaisseau;
 
 	public SpaceInvaders(int longueur, int hauteur) {
 		this.longueur = longueur;
@@ -12,13 +17,46 @@ public class SpaceInvaders {
 
 	@Override
 	public String toString() {
+		return recupererEspaceJeuDansChaineASCII();
+	}
+
+	public String recupererEspaceJeuDansChaineASCII() {
 		StringBuilder espaceDeJeu = new StringBuilder();
-		for (int i = 0; i < hauteur; i++) {
-			for (int j = 0; j < longueur; j++) {
-				espaceDeJeu.append(".");
+		for (int y = 0; y < hauteur; y++) {
+			for (int x = 0; x < longueur; x++) {
+				espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
 			}
 			espaceDeJeu.append("\n");
 		}
 		return espaceDeJeu.toString();
+	}
+
+	private char recupererMarqueDeLaPosition(int x, int y) {
+		char marque;
+		if (aUnVaisseauQuiOccupeLaPosition(x, y))
+			marque =MARQUE_VAISSEAU;
+		else
+			marque = MARQUE_VIDE;
+		return marque;
+	}
+
+	private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
+		return aUnVaisseau() && vaisseau.occupeLaPosition(x,y);
+	}
+
+	private boolean aUnVaisseau() {
+		return vaisseau != null;
+	}
+
+	public void positionnerUnNouveauVaisseau(int x, int y) {
+		
+		if (!estDansEspaceJeu(x, y) )
+			throw new HorsEspaceJeuException("Vous êtes en dehors de l'espace de jeu");
+		
+		vaisseau = new Vaisseau(x, y);
+	}
+
+	private boolean estDansEspaceJeu(int x, int y) {
+		return x < longueur && x >= 0 && y < hauteur && y >= 0;
 	}
 }
---
+++ src/main/java/fr/unilim/iut/spaceinvaders/utils/HorsEspaceJeuException.java
@@ -0,0 +1,10 @@
+package fr.unilim.iut.spaceinvaders.utils;
+
+public class HorsEspaceJeuException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public HorsEspaceJeuException(String message) {
+		super(message);
+	}
+}
