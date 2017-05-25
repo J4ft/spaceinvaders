55e3f493571aa469fa982d52571eb18663ca1710
p-rev <reveillere.pierrick@gmail.com>
2017-05-25 18:34:02 +0200

    modification vitesse

--- src/main/java/fr/unilim/iut/spaceinvaders/model/Constante.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/model/Constante.java
@@ -4,13 +4,13 @@ public class Constante {
 	public static final Dimension ECRAN = new Dimension(250, 400);
 	
 	public static final Dimension VAISSEAU = new Dimension(33, 20);
-	public static final int VAISSEAU_VITESSE = 14;
+	public static final int VAISSEAU_VITESSE = 5;
 	
 	public static final Dimension MISSILE = new Dimension(3, 6);
-	public static final int MISSILE_VITESSE = 14;
+	public static final int MISSILE_VITESSE = 7;
 	
 	public static final Dimension ENVAHISSEUR = new Dimension(27, 15);
-	public static final int ENVAHISSEUR_VITESSE = 5;
+	public static final int ENVAHISSEUR_VITESSE = 1;
 
 	public static final char MARQUE_VIDE = '.';
 	public static final char MARQUE_VAISSEAU = 'V';
